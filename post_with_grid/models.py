6362de1591daf3b842a7b8d39069cd53717d7638
Antoine HINGE <antoine.hinge@vectaury.io>
2020-05-13 23:17:44 +0200

    Changed path of the FilePathField

--- post_with_grid/models.py
+++ post_with_grid/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from main.settings import FILE_PATH_FIELD_DIRECTORY
 
 # Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    grid_path = models.FilePathField(path="/home/antoine/Documents")
+    grid_path = models.FilePathField(path=FILE_PATH_FIELD_DIRECTORY, match=".*.puz")
