3b02dbb1f82ad222343f8dc6ec230467d263ca41
Antoine HINGE <antoine.hinge@vectaury.io>
2020-05-13 20:01:26 +0200

    Fixed grid app url

--- post_with_grid/urls.py
+++ post_with_grid/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.project_index, name="project_index"),
-    path("<int:pk>/", views.project_detail, name="project_detail"),
+    path("", views.project_index, name="grid_index"),
+    path("<int:pk>/", views.project_detail, name="grid_detail"),
 ]
