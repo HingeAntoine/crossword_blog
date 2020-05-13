e05c6162f74232b31d3a2b46976fbba16a66639a
Antoine HINGE <antoine.hinge@vectaury.io>
2020-05-13 19:26:58 +0200

    Added simple blog post structure

--- post_with_grid/models.py
+++ post_with_grid/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    grid_path = models.FilePathField(path="/puzzle")
