80a6b03a11cf1475cc5f1f1892ff30ad663ae432
Antoine HINGE <antoine.hinge@vectaury.io>
2020-05-13 22:41:13 +0200

    Added admin support for Project class

--- post_with_grid/admin.py
+++ post_with_grid/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from post_with_grid.models import Project
 
 # Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+    fields = ('title', 'description', 'grid_path')
+
+admin.site.register(Project, ProjectAdmin)
--- post_with_grid/models.py
+++ post_with_grid/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    grid_path = models.FilePathField(path="/puzzle")
+    grid_path = models.FilePathField(path="/home/antoine/Documents")
