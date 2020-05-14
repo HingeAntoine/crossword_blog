ee18abfe30831d9b6488c06162ea57ec9f48790c
Antoine HINGE <antoine.hinge@vectaury.io>
2020-05-14 09:32:47 +0200

    Added static folder outside of project

--- post_with_grid/urls.py
+++ post_with_grid/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.project_index, name="project_index"),
     path("<int:pk>/", views.project_detail, name="project_detail"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
