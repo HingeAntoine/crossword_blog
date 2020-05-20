6f81f034b215ff4196ef965529a37f9dc9db16c2
Antoine HINGE <antoine.hinge@vectaury.io>
2020-05-20 18:53:23 +0200

    Removed bottom text reference from crossword.js

--- post_with_grid/static/js/crosswords.js
+++ post_with_grid/static/js/crosswords.js
@@ -166,7 +166,6 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
                 this.copyright = puzzle.copyright;
                 text += "<br>" + this.copyright;
             }
-            this.bottom_text.html(text);
         }
 
         this.grid_width = puzzle.width;
