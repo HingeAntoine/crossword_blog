b1926b177832f9b6201981093ced3275b90c58ca
Antoine HINGE <antoine.hinge@vectaury.io>
2020-05-22 18:08:38 +0200

    Clean settings (static files)

--- main/settings.py
+++ main/settings.py
@@ -135,10 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Media files
