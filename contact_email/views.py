51f9f488028fcd870345662bcdf13a03f2dabf98
Antoine HINGE <antoine.hinge@vectaury.io>
2020-05-27 14:04:29 +0200

    Changed receiver email in form

--- contact_email/views.py
+++ contact_email/views.py
@@ -14,7 +14,7 @@ def contact_view(request):
             from_email = form.cleaned_data['from_email']
             message = form.cleaned_data['message']
             try:
-                send_mail(subject, message, from_email, ['admin@example.com'])
+                send_mail(subject, message, from_email, ['postmaster@casevide.fr'])
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
             return redirect('success')
