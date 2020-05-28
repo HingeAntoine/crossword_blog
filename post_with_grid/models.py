f46666f008abc3d6c2acd988cb47bc3aed3ab484
Antoine HINGE <antoine.hinge@vectaury.io>
2020-05-28 22:21:30 +0200

    Add solve count to model and grid detail

--- post_with_grid/models.py
+++ post_with_grid/models.py
@@ -8,3 +8,4 @@ class Project(models.Model):
     grid_file = models.FileField(upload_to='puzzles/')
     author = models.CharField(max_length=25)
     date_created = models.DateField(auto_now_add=True)
+    solve_count = models.IntegerField(default=0)
