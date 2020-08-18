7831597a42bf37e5284ee1ef222c6a1fc90dfe43
Antoine HINGE <antoine.hinge@vectaury.io>
2020-08-18 22:04:48 +0200

    Added admin for edito class

--- post_with_grid/admin.py
+++ post_with_grid/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from post_with_grid.models import Project
+from post_with_grid.models import Edito
 
 
 # Register your models here.
 class ProjectAdmin(admin.ModelAdmin):
-    fields = ('title', 'description', 'grid_file', 'author')
+    fields = ("title", "description", "grid_file", "author")
+
+
+class EditoAdmin(admin.ModelAdmin):
+    fields = ("title", "content")
 
 
 admin.site.register(Project, ProjectAdmin)
+admin.site.register(Edito, EditoAdmin)
