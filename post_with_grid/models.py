ef86fc45e50cb1477b639909caaa9c3a222c1401
Antoine HINGE <antoine.hinge@vectaury.io>
2020-08-18 21:59:05 +0200

    Created edito field when displaying grid

--- post_with_grid/models.py
+++ post_with_grid/models.py
@@ -4,7 +4,13 @@ from django.db import models
 # Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=100)
-    grid_file = models.FileField(upload_to='puzzles/')
+    grid_file = models.FileField(upload_to="puzzles/")
     author = models.CharField(max_length=25)
     date_created = models.DateField(auto_now_add=True)
     solve_count = models.IntegerField(default=0)
+
+
+class Edito(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date_created = models.DateField(auto_now_add=True)
--- post_with_grid/views.py
+++ post_with_grid/views.py
@@ -1,33 +1,29 @@
 from django.db.models import F
 from django.shortcuts import render
 from post_with_grid.models import Project
+from post_with_grid.models import Edito
 
 
 def project_index(request):
-    projects = Project.objects.all().order_by('-date_created', '-title')
-    context = {
-        'projects': projects
-    }
+    projects = Project.objects.all().order_by("-date_created", "-title")
+    edito = Edito.object.first()
+    context = {"projects": projects, "edito": edito}
 
-    return render(request, 'grid_index.html', context)
+    return render(request, "grid_index.html", context)
 
 
 def project_detail(request, pk):
     project = Project.objects.get(pk=pk)
-    context = {
-        'project': project
-    }
+    context = {"project": project}
 
-    if request.method == 'POST':
+    if request.method == "POST":
         Project.objects.filter(pk=pk).update(solve_count=F("solve_count") + 1)
 
-    return render(request, 'grid_detail.html', context)
+    return render(request, "grid_detail.html", context)
 
 
 def project_archives(request):
-    projects = Project.objects.all().order_by('-date_created', '-title')
-    context = {
-        'projects': projects
-    }
+    projects = Project.objects.all().order_by("-date_created", "-title")
+    context = {"projects": projects}
 
-    return render(request, 'grid_archives.html', context)
+    return render(request, "grid_archives.html", context)
