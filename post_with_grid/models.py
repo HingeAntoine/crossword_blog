eb22422c9ddc601ae942d554227ee856637fba2e
Antoine HINGE <antoine.hinge@vectaury.io>
2020-08-18 19:50:12 +0200

    Removed grid description from models template

--- post_with_grid/models.py
+++ post_with_grid/models.py
@@ -4,7 +4,6 @@ from django.db import models
 # Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField()
     grid_file = models.FileField(upload_to='puzzles/')
     author = models.CharField(max_length=25)
     date_created = models.DateField(auto_now_add=True)
