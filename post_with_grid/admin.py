365f43f674b94a073dad5a783c2bf6ba57c133eb
Antoine HINGE <antoine.hinge@vectaury.io>
2020-12-17 09:43:58 +0100

    Read puzzle file and choose right width/height

--- post_with_grid/admin.py
+++ post_with_grid/admin.py
@@ -1,10 +1,11 @@
+from django.conf import settings
+
 from django.contrib import admin
 from post_with_grid.models import Project
 from post_with_grid.models import Edito
 
-from django.conf import settings
-
 from PIL import Image, ImageDraw
+import puz
 
 
 class ProjectAdmin(admin.ModelAdmin):
@@ -21,19 +22,19 @@ class ProjectAdmin(admin.ModelAdmin):
         # Create new object
         super(ProjectAdmin, self).save_model(request, obj, form, change)
 
+        # Create constant grid preview
         if "grid_file" in form.changed_data:
+            # Read puzzle
+            p = puz.read(settings.MEDIA_ROOT + "/" + str(obj.grid_file))
+
             # Init image with the right size
-            grid_width, grid_height = (7, 7)
-            img = Image.new("RGB", (5 * grid_width, 5 * grid_width), color="white")
+            img = Image.new("RGB", (5 * p.width, 5 * p.height), color="white")
 
             # Draw on image
             d = ImageDraw.Draw(img)
             d.rectangle([0, 0, 5, 5], fill="black")
 
             # Save image
-            print(obj.pk)
-            print(obj.grid_file)
-
             img_path = f"/preview/{obj.pk}.png"
             img.save(settings.MEDIA_ROOT + img_path)
 
