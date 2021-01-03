b5ab4157a07fb1ace3a63d56b9bef2b7285bd106
Antoine HINGE <antoine.hinge@vectaury.io>
2021-01-03 13:12:52 +0100

    Added admin fields for new authors

--- author_page/admin.py
+++ author_page/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from author_page.models import Author
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    def get_fields(self, request, obj=None):
+        if obj is None:
+            return ["name", "display_name", "bio"]
+        return ["display_name", "bio"]
+
+
+admin.site.register(Author, AuthorAdmin)
--- author_page/models.py
+++ author_page/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=50, primary_key=True)
     display_name = models.CharField(max_length=100)
-    bio = models.TextField
+    bio = models.TextField(default="")
