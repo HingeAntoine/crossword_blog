c4990a6b1923f5649740130d77d4cb8d248bc0e3
Antoine HINGE <antoine.hinge@vectaury.io>
2021-01-08 16:36:55 +0100

    Added last grids to author profiles

--- author_page/views.py
+++ author_page/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from author_page.models import Author
+from post_with_grid.models import Project
 
 
 def author_page(request, name):
     author = Author.objects.get(name=name)
-    context = {"author": author}
+    context = {
+        "author": author,
+        "grids": Project.objects.filter(author_key=name).order_by("-date_created")[:6],
+    }
 
     return render(request, "author_page.html", context)
