90b92da0218fd6dd86defead8a097eea0ea24e7c
Antoine HINGE <antoine.hinge@vectaury.io>
2021-03-04 19:47:45 +0100

    Put grid_view in an html import

--- author_page/views.py
+++ author_page/views.py
@@ -7,7 +7,9 @@ def author_page(request, name):
     author = Author.objects.get(name=name)
     context = {
         "author": author,
-        "grids": Project.objects.filter(author_key=name).order_by("-date_created")[:6],
+        "projects": Project.objects.filter(author_key=name).order_by("-date_created")[
+            :6
+        ],
     }
 
     return render(request, "author_page.html", context)
