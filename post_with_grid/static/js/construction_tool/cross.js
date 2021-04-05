74ef978b9cafcd74a0ed19169b36b625c11296f2
Antoine HINGE <antoine.hinge@vectaury.io>
2021-04-05 17:53:07 +0200

    Retooled css to be more clean

--- post_with_grid/static/js/construction_tool/cross.js
+++ post_with_grid/static/js/construction_tool/cross.js
@@ -183,7 +183,7 @@ class Toolbar {
 class Interface {
   constructor(rows, cols) {
     this.grid = new Grid(rows, cols);
-    this.sidebar;
+    //this.sidebar;
     this.toolbar = new Toolbar("toolbar");
 
     this.row = 0;
