1465605cb95309215fe1a2ee5f9ba6cc7abf3ed2
Antoine HINGE <antoine.hinge@vectaury.io>
2021-08-27 12:08:43 +0200

    Functionnal score carousel

--- main/views.py
+++ main/views.py
@@ -16,15 +16,23 @@ def project_index(request):
     edito = Edito.objects.last()
 
     # Retrieve scores
-    scores = get_scores(projects[0].pk)
-    grid_type = get_type(projects[0].pk)
+    scores_vector = [
+        get_scores(projects[0].pk),
+        get_scores(projects[1].pk),
+        get_scores(projects[2].pk),
+    ]
+    grid_type_vector = [
+        get_type(projects[0].pk),
+        get_type(projects[1].pk),
+        get_type(projects[2].pk),
+    ]
 
     # Create context
     context = {
         "projects": projects,
         "edito": edito,
-        "scores": scores,
-        "type": grid_type,
+        "scores_vector": scores_vector,
+        "type_vector": grid_type_vector,
     }
 
     return render(request, "grid_index.html", context)
