fbc2cb67790a02d4502e17c88f53b8cc338668eb
Antoine HINGE <antoine.hinge@vectaury.io>
2021-12-19 14:37:40 +0100

    Functionnal stub of blog posts

--- blog_posts/urls.py
+++ blog_posts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("<str:url_blog>/", views.display_blog_post, name="blog_post"),
     # path("tuto/", views.construction, name="construction_tuto"),
     # path("outil/", views.construction_tool, name="construction_tool"),
 ]
