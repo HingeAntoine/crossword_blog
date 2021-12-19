4c333a6f7dd59e5133d6dea8422ea4c8962b83f5
Antoine HINGE <antoine.hinge@vectaury.io>
2021-12-19 15:12:43 +0100

    Redirect simple grid url to archives

--- post_with_grid/urls.py
+++ post_with_grid/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.shortcuts import redirect
 from . import views
 
 urlpatterns = [
+    # GET TO GRID ARCHIVES
+    path("", lambda request: redirect("archives/", permanent=False)),
     path("archives/", views.project_archives, name="project_archives"),
+    # GRID AND ASSOCIATED OBJECTS (COMMENTS & RANKING)
     path("<int:pk>/", views.project_detail, name="project_detail"),
     path("<int:pk>/classement/", views.project_ranking, name="project_ranking"),
     path("<int:pk>/comments/", views.grid_comments, name="grid_comments"),
