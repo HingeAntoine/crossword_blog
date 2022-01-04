7aab31835285fe17dd2befbeb648a15f3ff9a70d
Antoine HINGE <antoine.hinge@vectaury.io>
2022-01-04 17:04:07 +0100

    Show most recent post on homepage

--- main/views.py
+++ main/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 
 from author_page.models import Author
 from blog_posts.models import BlogPost
-from post_with_grid.models import Project, Edito
+from post_with_grid.models import Project, Score
 from post_with_grid.views import get_scores, get_type
+
+import datetime
 
 
 #################
@@ -16,7 +18,7 @@ def project_index(request):
     max_index = 6
 
     projects = Project.objects.all().order_by("-date_created", "-title")[:max_index]
-    blog_post = BlogPost.objects.last()
+    blog_post = BlogPost.objects.order_by("-date_created", "-title")[0]
 
     # Retrieve scores
     grilles_vector = [
@@ -36,6 +38,30 @@ def project_index(request):
     }
 
     return render(request, "grid_index.html", context)
+
+
+##########
+# SCORES #
+##########
+
+
+def monthly_score_summary(request):
+
+    # Get all scores in September
+    start_date = datetime.date(year=2021, month=9, day=1)
+    end_date = datetime.date(year=2021, month=9, day=30)
+    grid_list = Project.objects.filter(
+        date_created__gte=start_date, date_created__lte=end_date
+    ).values_list("id", flat=True)
+    print(grid_list)
+
+    scores = Score.objects.filter(grid__in=grid_list)
+    print(scores)
+
+    # Create context
+    context = {}
+
+    return render(request, "monthly_pantheon.html", context)
 
 
 #########
